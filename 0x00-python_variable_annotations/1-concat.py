bc411496496ecea8b640eef9a021f7c548e22c46
mohamed kamal <kmido9729@gmail.com>
2024-06-07 22:10:42 +0200

    python is funn

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""type-annotated function concat that takes a string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """this func with two strings"""
+    return str1 + str2
